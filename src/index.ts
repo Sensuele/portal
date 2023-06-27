3b09411bae273d78d9ec767c7b884b64c3497bbf
e.druzhnyaev <druzhniaev2@gmail.com>
2023-06-27 11:17:39 +0600

    test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-document.body.innerHTML = '<div> hello</div>'
+import { createApp } from 'vue'
+// import the root component App from a single-file component.
+import App from './App.vue'
+const app = createApp(App);
+app.mount('#app');
