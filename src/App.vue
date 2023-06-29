b2d14885c8cf4e1fe4ea8f7522db8cdded7467af
e.druzhnyaev <druzhniaev2@gmail.com>
2023-06-29 16:09:57 +0600

    helper className

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="['app', theme]">
+    <div :class="[classNames('app', {}, [theme])]">
         <router-link to="/about">Go to About</router-link>
         <button @click="toggleTheme">{{ theme }}</button>
         <router-view />
@@ -8,6 +8,7 @@
 
 <script setup lang="ts">
 import { useTheme } from './hooks/useTheme';
+import { classNames } from './helpers/classNames';
 
 const { theme, toggleTheme} = useTheme();
 </script>
